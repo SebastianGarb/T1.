d3ec0843842d3b503749f77a2b696b3726382390
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-09-19 09:20:59 -0500

    T1.

--- P2/Program.cs
+++ P2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int placa = 0;
+string color;
+
+Console.WriteLine("Ingrese los colores de las placas");
+color = Console.ReadLine().ToUpper().Trim();
+while (color == "F")
+{
+    color = Console.ReadLine().ToUpper().Trim();
+    if (color == "VERDE")
+    {
+        for (placa++; placa < 3 && placa > 0;) ;
+    }
+    else if (color == "AZUL")
+    {
+        for (placa++ ; placa < 6 && placa > 4;) ;
+    }
+    else if (color == "ROJO")
+    {
+        for (placa++ ; placa > 7 && placa < 9;) ;
+    }
+   
+        
+        for (Console.WriteLine($"{placa++}, "); color == "F";);
+        
+    
+}
+Console.ReadKey();
